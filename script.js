27ca266034554af7be41d7c651d0b7f09c91c43f
Raul-Araujo <raul31_lpcn@hotmail.com>
2022-07-20 19:39:25 -0300

    function created to run a single round of the game

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ function getPlayerChoice(){
     return playerOptions.toUpperCase();
 }
 
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == "ROCK" && computerSelection == "SCISSORS"){
+        return "You beat computer!";
+    } else if (playerSelection == "PAPER" && computerSelection == "SCISSORS"){
+        return "Your choice: " + playerSelection + " Computer's choice: " + computerSelection + "Computer beats you!";
+    } else if (playerSelection == "SCISSORS" && computerSelection == "SCISSORS"){
+            return "Nothing happens, same choice.";
+    } else if(playerSelection == "ROCK" && computerSelection == "PAPER"){
+        return "Computer beats you!";
+    } else if (playerSelection == "PAPER" && computerSelection == "PAPER"){
+        return "Nothing happens, same choice.";
+    } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER"){
+            return "You beat computer!";
+    } else if(playerSelection == "ROCK" && computerSelection == "ROCK"){
+        return "Nothing happens, same choice.";
+    } else if (playerSelection == "PAPER" && computerSelection == "ROCK"){
+        return "You beat computer!";
+    } else if (playerSelection == "SCISSORS" && computerSelection == "ROCK"){
+            return "Computer beats you!";
+    } else if(playerSelection != "PAPER" || playerSelection != "SCISSORS" || playerSelection != "ROCK") {
+        return "Wrong Choice, mate, you loose."
+    }
+}
+
+    for (i=0; i < 5; i++){
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
